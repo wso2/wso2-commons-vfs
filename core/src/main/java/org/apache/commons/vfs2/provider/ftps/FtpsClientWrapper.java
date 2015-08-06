d29c6de5495cd0bb9ba6883011e0fe032610f736
virajsenevirathne <viraj.uom@gmail.com>
2015-08-06 14:52:12 +0530

    "Formatting FIx"

--- core/src/main/java/org/apache/commons/vfs2/provider/ftps/FtpsClientWrapper.java
+++ core/src/main/java/org/apache/commons/vfs2/provider/ftps/FtpsClientWrapper.java
@@ -48,18 +48,18 @@ class FtpsClientWrapper implements FtpClient
 
     private FTPSClient ftpClient = null;
 
-    FtpsClientWrapper(final GenericFileName root, final FileSystemOptions fileSystemOptions,
-                      Integer connectTimeout) throws FileSystemException {
+    FtpsClientWrapper(final GenericFileName root, final FileSystemOptions fileSystemOptions, Integer connectTimeout)
+            throws FileSystemException {
         this.root = root;
         this.fileSystemOptions = fileSystemOptions;
         this.connectTimeout = connectTimeout;
         getFtpsClient(); // fail-fast
     }
 
-    FtpsClientWrapper(final GenericFileName root, final FileSystemOptions fileSystemOptions)
-            throws FileSystemException {
-        this(root, fileSystemOptions, null);
-    }
+	FtpsClientWrapper(final GenericFileName root, final FileSystemOptions fileSystemOptions)
+			throws FileSystemException {
+		this(root, fileSystemOptions, null);
+	}
 
     public GenericFileName getRoot()
     {
@@ -80,15 +80,14 @@ class FtpsClientWrapper implements FtpClient
         {
             authData = UserAuthenticatorUtils.authenticate(fileSystemOptions, FtpsFileProvider.AUTHENTICATOR_TYPES);
 
-            return FtpsClientFactory.createConnection(rootName.getHostName(),
-                rootName.getPort(),
-                UserAuthenticatorUtils.getData(authData, UserAuthenticationData.USERNAME,
-                                               UserAuthenticatorUtils.toChar(rootName.getUserName())),
-                UserAuthenticatorUtils.getData(authData, UserAuthenticationData.PASSWORD,
-                                               UserAuthenticatorUtils.toChar(rootName.getPassword())),
-                rootName.getPath(),
-                getFileSystemOptions(),
-                connectTimeout);
+            char[] userName = UserAuthenticatorUtils.getData(authData, UserAuthenticationData.USERNAME,
+                                                             UserAuthenticatorUtils.toChar(rootName.getUserName()));
+
+            char[] password = UserAuthenticatorUtils.getData(authData, UserAuthenticationData.PASSWORD,
+                                                             UserAuthenticatorUtils.toChar(rootName.getPassword()));
+
+            return FtpsClientFactory.createConnection(rootName.getHostName(), rootName.getPort(), userName, password,
+                                                      rootName.getPath(), getFileSystemOptions(), connectTimeout);
         }
         finally
         {
