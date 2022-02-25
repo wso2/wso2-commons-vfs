3139f2609105a0b658a2d0e7696873f7ed990c7f
Arunan Sugunakumar <arunans23@gmail.com>
2022-02-25 17:12:19 +0530

    Add code review suggestions

--- commons-vfs2-sandbox/src/test/java/org/apache/commons/vfs2/provider/smb2/test/Smb2FileNameParserTest.java
+++ commons-vfs2-sandbox/src/test/java/org/apache/commons/vfs2/provider/smb2/test/Smb2FileNameParserTest.java
@@ -32,7 +32,7 @@ public class Smb2FileNameParserTest {
     private final static String testPasswordUE = /* printable ascii chars */
             "%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2D%2E%2F%30%31%32%33%34%35%36%37%38%39%3A%3B%3C%3D%3E%3F%40%41%42%43%44%45%46%47%48%49%4A%4B%4C%4D%4E%4F" +
                     "%50%51%52%53%54%55%56%57%58%59%5A%5B%5C%5D%5E%5F%60%61%62%63%64%65%66%67%68%69%6A%6B%6C%6D%6E%6F%70%71%72%73%74%75%76%77%78%79%7A%7B%7C%7D%7E";
-    private final static String testPath = /* printable ascii chars without / and \ */
+    private final static String testPath = 
             "%20%21%22%23%24%25%26%27%28%29%2A%2B%2C%2D%2E%2F%30%31%32%33%34%35%36%37%38%39%3A%3B%3C%3D%3E%3F%40%41%42%43%44%45%46%47%48%49%4A%4B%4C%4D%4E%4F" +
                     "%50%51%52%53%54%55%56%57%58%59%5A%5B%5C%5D%5E%5F%60%61%62%63%64%65%66%67%68%69%6A%6B%6C%6D%6E%6F%70%71%72%73%74%75%76%77%78%79%7A%7B%7C%7D%7E";
     private final static String testURIString = "smb2://WORKGROUP;USER:" + testPasswordUE + "@example.com/ShareName/Folder1/" + testPath + "/Folder2/?somequery=true&another=query#fragment";
