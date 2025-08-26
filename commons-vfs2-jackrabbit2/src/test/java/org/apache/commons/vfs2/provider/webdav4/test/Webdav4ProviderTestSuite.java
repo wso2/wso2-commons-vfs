f9e983f118d3d12d1f27f2993d6e3b04f0cf467c
lnash94 <sumudunissanke@gmail.com>
2025-08-26 10:24:07 +0530

    Disable tests and ignore java doc errors

--- commons-vfs2-jackrabbit2/src/test/java/org/apache/commons/vfs2/provider/webdav4/test/Webdav4ProviderTestSuite.java
+++ commons-vfs2-jackrabbit2/src/test/java/org/apache/commons/vfs2/provider/webdav4/test/Webdav4ProviderTestSuite.java
@@ -14,69 +14,69 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//package org.apache.commons.vfs2.provider.webdav4.test;
-//
-//import org.apache.commons.vfs2.ContentTests;
-//import org.apache.commons.vfs2.LastModifiedTests;
-//import org.apache.commons.vfs2.NamingTests;
-//import org.apache.commons.vfs2.ProviderCacheStrategyTests;
-//import org.apache.commons.vfs2.ProviderDeleteTests;
-//import org.apache.commons.vfs2.ProviderRandomReadTests;
-//import org.apache.commons.vfs2.ProviderRandomReadWriteTests;
-//import org.apache.commons.vfs2.ProviderReadTests;
-//import org.apache.commons.vfs2.ProviderRenameTests;
-//import org.apache.commons.vfs2.ProviderTestConfig;
-//import org.apache.commons.vfs2.ProviderTestSuite;
-//import org.apache.commons.vfs2.ProviderWriteAppendTests;
-//import org.apache.commons.vfs2.ProviderWriteTests;
-//import org.apache.commons.vfs2.UriTests;
-//import org.apache.commons.vfs2.UrlStructureTests;
-//import org.apache.commons.vfs2.UrlTests;
-//
-///**
-// * The suite of tests for a file system.
-// */
-//public class Webdav4ProviderTestSuite extends ProviderTestSuite {
-//    /**
-//     * Adds the tests for a file system to this suite.
-//     */
-//    public Webdav4ProviderTestSuite(final ProviderTestConfig providerConfig) throws Exception {
-//        this(providerConfig, "", false, false);
-//    }
-//
-//    /**
-//     * Adds the tests for a file system to this suite. Provider has an empty directory.
-//     */
-//    public Webdav4ProviderTestSuite(final ProviderTestConfig providerConfig, final boolean addEmptyDir)
-//            throws Exception {
-//        this(providerConfig, "", false, addEmptyDir);
-//    }
-//
-//    protected Webdav4ProviderTestSuite(final ProviderTestConfig providerConfig, final String prefix,
-//            final boolean nested, final boolean addEmptyDir) throws Exception {
-//        super(providerConfig, prefix, nested, addEmptyDir);
-//    }
-//
-//    /**
-//     * Adds base tests - excludes the nested test cases.
-//     */
-//    @Override
-//    protected void addBaseTests() throws Exception {
-//        addTests(ProviderCacheStrategyTests.class);
-//        addTests(UriTests.class);
-//        addTests(NamingTests.class);
-//        addTests(ContentTests.class);
-//        addTests(ProviderReadTests.class);
-//        addTests(ProviderRandomReadTests.class);
-//        addTests(ProviderWriteTests.class);
-//        addTests(ProviderWriteAppendTests.class);
-//        addTests(ProviderRandomReadWriteTests.class);
-//        addTests(ProviderRenameTests.class);
-//        addTests(ProviderDeleteTests.class);
-//        addTests(LastModifiedTests.class);
-//        addTests(UrlTests.class);
-//        addTests(UrlStructureTests.class);
-//        // The class loader test requires the classes be uploaded to the webdav repo.
-//        // addTests(VfsClassLoaderTests.class);
-//    }
-//}
+package org.apache.commons.vfs2.provider.webdav4.test;
+
+import org.apache.commons.vfs2.ContentTests;
+import org.apache.commons.vfs2.LastModifiedTests;
+import org.apache.commons.vfs2.NamingTests;
+import org.apache.commons.vfs2.ProviderCacheStrategyTests;
+import org.apache.commons.vfs2.ProviderDeleteTests;
+import org.apache.commons.vfs2.ProviderRandomReadTests;
+import org.apache.commons.vfs2.ProviderRandomReadWriteTests;
+import org.apache.commons.vfs2.ProviderReadTests;
+import org.apache.commons.vfs2.ProviderRenameTests;
+import org.apache.commons.vfs2.ProviderTestConfig;
+import org.apache.commons.vfs2.ProviderTestSuite;
+import org.apache.commons.vfs2.ProviderWriteAppendTests;
+import org.apache.commons.vfs2.ProviderWriteTests;
+import org.apache.commons.vfs2.UriTests;
+import org.apache.commons.vfs2.UrlStructureTests;
+import org.apache.commons.vfs2.UrlTests;
+
+/**
+ * The suite of tests for a file system.
+ */
+public class Webdav4ProviderTestSuite extends ProviderTestSuite {
+    /**
+     * Adds the tests for a file system to this suite.
+     */
+    public Webdav4ProviderTestSuite(final ProviderTestConfig providerConfig) throws Exception {
+        this(providerConfig, "", false, false);
+    }
+
+    /**
+     * Adds the tests for a file system to this suite. Provider has an empty directory.
+     */
+    public Webdav4ProviderTestSuite(final ProviderTestConfig providerConfig, final boolean addEmptyDir)
+            throws Exception {
+        this(providerConfig, "", false, addEmptyDir);
+    }
+
+    protected Webdav4ProviderTestSuite(final ProviderTestConfig providerConfig, final String prefix,
+            final boolean nested, final boolean addEmptyDir) throws Exception {
+        super(providerConfig, prefix, nested, addEmptyDir);
+    }
+
+    /**
+     * Adds base tests - excludes the nested test cases.
+     */
+    @Override
+    protected void addBaseTests() throws Exception {
+        addTests(ProviderCacheStrategyTests.class);
+        addTests(UriTests.class);
+        addTests(NamingTests.class);
+        addTests(ContentTests.class);
+        addTests(ProviderReadTests.class);
+        addTests(ProviderRandomReadTests.class);
+        addTests(ProviderWriteTests.class);
+        addTests(ProviderWriteAppendTests.class);
+        addTests(ProviderRandomReadWriteTests.class);
+        addTests(ProviderRenameTests.class);
+        addTests(ProviderDeleteTests.class);
+        addTests(LastModifiedTests.class);
+        addTests(UrlTests.class);
+        addTests(UrlStructureTests.class);
+        // The class loader test requires the classes be uploaded to the webdav repo.
+        // addTests(VfsClassLoaderTests.class);
+    }
+}
--- commons-vfs2-jackrabbit2/src/test/java/org/apache/commons/vfs2/provider/webdav4/test/Webdav4VersioningTests.java
+++ commons-vfs2-jackrabbit2/src/test/java/org/apache/commons/vfs2/provider/webdav4/test/Webdav4VersioningTests.java
@@ -14,137 +14,137 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-//package org.apache.commons.vfs2.provider.webdav4.test;
-//
-//import java.io.OutputStream;
-//import java.nio.charset.StandardCharsets;
-//import java.util.Map;
-//
-//import org.apache.commons.vfs2.AbstractProviderTestCase;
-//import org.apache.commons.vfs2.FileObject;
-//import org.apache.commons.vfs2.FileSystemOptions;
-//import org.apache.commons.vfs2.FileType;
-//import org.apache.commons.vfs2.Selectors;
-//import org.apache.commons.vfs2.provider.GenericURLFileName;
-//import org.apache.commons.vfs2.provider.webdav4.Webdav4FileSystemConfigBuilder;
-//import org.apache.jackrabbit.webdav.version.DeltaVConstants;
-//import org.apache.jackrabbit.webdav.version.VersionControlledResource;
-//import org.junit.Test;
-//
-///**
-// * Test to verify WebDAV Versioning support
-// */
-//public class Webdav4VersioningTests extends AbstractProviderTestCase {
-//
-//    /**
-//     * Sets up a scratch folder for the test to use.
-//     */
-//    protected FileObject createScratchFolder() throws Exception {
-//        final FileObject scratchFolder = getWriteFolder();
-//
-//        // Make sure the test folder is empty
-//        scratchFolder.delete(Selectors.EXCLUDE_SELF);
-//        scratchFolder.createFolder();
-//
-//        return scratchFolder;
-//    }
-//
-//    @Test
-//    public void testVersioning() throws Exception {
-//        final FileObject scratchFolder = createScratchFolder();
-//        final FileSystemOptions opts = scratchFolder.getFileSystem().getFileSystemOptions();
-//        final Webdav4FileSystemConfigBuilder builder = (Webdav4FileSystemConfigBuilder) getManager()
-//                .getFileSystemConfigBuilder("webdav");
-//        builder.setVersioning(opts, true);
-//        final FileObject file = getManager().resolveFile(scratchFolder, "file1.txt", opts);
-//        final FileSystemOptions newOpts = file.getFileSystem().getFileSystemOptions();
-//        assertSame(opts, newOpts);
-//        assertTrue(builder.isVersioning(newOpts));
-//        assertFalse(file.exists());
-//        file.createFile();
-//        assertTrue(file.exists());
-//        assertSame(FileType.FILE, file.getType());
-//        assertTrue(file.isFile());
-//        assertEquals(0, file.getContent().getSize());
-//        assertTrue(file.getContent().isEmpty());
-//        assertFalse(file.isExecutable());
-//        assertFalse(file.isHidden());
-//        assertTrue(file.isReadable());
-//        assertTrue(file.isWriteable());
-//        Map<?, ?> map = file.getContent().getAttributes();
-//        final String name = ((GenericURLFileName) file.getName()).getUserName();
-//        assertTrue(map.containsKey(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
-//        if (name != null) {
-//            assertEquals(name, map.get(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
-//        }
-//        assertTrue(map.containsKey(VersionControlledResource.CHECKED_IN.toString()));
-//
-//        // Create the source file
-//        final String content = "Here is some sample content for the file.  Blah Blah Blah.";
-//
-//        try (OutputStream os = file.getContent().getOutputStream()) {
-//            os.write(content.getBytes(StandardCharsets.UTF_8));
-//        }
-//        assertSameContent(content, file);
-//        map = file.getContent().getAttributes();
-//        assertTrue(map.containsKey(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
-//        if (name != null) {
-//            assertEquals(name, map.get(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
-//        }
-//        assertTrue(map.containsKey(VersionControlledResource.CHECKED_IN.toString()));
-//        builder.setVersioning(opts, false);
-//    }
-//
-//    @Test
-//    public void testVersioningWithCreator() throws Exception {
-//        final FileObject scratchFolder = createScratchFolder();
-//        final FileSystemOptions opts = scratchFolder.getFileSystem().getFileSystemOptions();
-//        final Webdav4FileSystemConfigBuilder builder = (Webdav4FileSystemConfigBuilder) getManager()
-//                .getFileSystemConfigBuilder("webdav");
-//        builder.setVersioning(opts, true);
-//        builder.setCreatorName(opts, "testUser");
-//        final FileObject file = getManager().resolveFile(scratchFolder, "file1.txt", opts);
-//        final FileSystemOptions newOpts = file.getFileSystem().getFileSystemOptions();
-//        assertSame(opts, newOpts);
-//        assertTrue(builder.isVersioning(newOpts));
-//        assertFalse(file.exists());
-//        file.createFile();
-//        assertTrue(file.exists());
-//        assertSame(FileType.FILE, file.getType());
-//        assertTrue(file.isFile());
-//        assertEquals(0, file.getContent().getSize());
-//        assertTrue(file.getContent().isEmpty());
-//        assertFalse(file.isExecutable());
-//        assertFalse(file.isHidden());
-//        assertTrue(file.isReadable());
-//        assertTrue(file.isWriteable());
-//        Map<?, ?> map = file.getContent().getAttributes();
-//        final String name = ((GenericURLFileName) file.getName()).getUserName();
-//        assertTrue(map.containsKey(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
-//        assertEquals("testUser", map.get(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
-//        if (name != null) {
-//            assertTrue(map.containsKey(DeltaVConstants.COMMENT.toString()));
-//            assertEquals("Modified by user " + name, map.get(DeltaVConstants.COMMENT.toString()));
-//        }
-//        assertTrue(map.containsKey(VersionControlledResource.CHECKED_IN.toString()));
-//
-//        // Create the source file
-//        final String content = "Here is some sample content for the file.  Blah Blah Blah.";
-//
-//        try (OutputStream os = file.getContent().getOutputStream()) {
-//            os.write(content.getBytes(StandardCharsets.UTF_8));
-//        }
-//        assertSameContent(content, file);
-//        map = file.getContent().getAttributes();
-//        assertTrue(map.containsKey(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
-//        assertEquals("testUser", map.get(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
-//        if (name != null) {
-//            assertTrue(map.containsKey(DeltaVConstants.COMMENT.toString()));
-//            assertEquals("Modified by user " + name, map.get(DeltaVConstants.COMMENT.toString()));
-//        }
-//        assertTrue(map.containsKey(VersionControlledResource.CHECKED_IN.toString()));
-//        builder.setVersioning(opts, false);
-//        builder.setCreatorName(opts, null);
-//    }
-//
-//}
+package org.apache.commons.vfs2.provider.webdav4.test;
+
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import org.apache.commons.vfs2.AbstractProviderTestCase;
+import org.apache.commons.vfs2.FileObject;
+import org.apache.commons.vfs2.FileSystemOptions;
+import org.apache.commons.vfs2.FileType;
+import org.apache.commons.vfs2.Selectors;
+import org.apache.commons.vfs2.provider.GenericURLFileName;
+import org.apache.commons.vfs2.provider.webdav4.Webdav4FileSystemConfigBuilder;
+import org.apache.jackrabbit.webdav.version.DeltaVConstants;
+import org.apache.jackrabbit.webdav.version.VersionControlledResource;
+import org.junit.Test;
+
+/**
+ * Test to verify WebDAV Versioning support
+ */
+public class Webdav4VersioningTests extends AbstractProviderTestCase {
+
+    /**
+     * Sets up a scratch folder for the test to use.
+     */
+    protected FileObject createScratchFolder() throws Exception {
+        final FileObject scratchFolder = getWriteFolder();
+
+        // Make sure the test folder is empty
+        scratchFolder.delete(Selectors.EXCLUDE_SELF);
+        scratchFolder.createFolder();
+
+        return scratchFolder;
+    }
+
+    @Test
+    public void testVersioning() throws Exception {
+        final FileObject scratchFolder = createScratchFolder();
+        final FileSystemOptions opts = scratchFolder.getFileSystem().getFileSystemOptions();
+        final Webdav4FileSystemConfigBuilder builder = (Webdav4FileSystemConfigBuilder) getManager()
+                .getFileSystemConfigBuilder("webdav");
+        builder.setVersioning(opts, true);
+        final FileObject file = getManager().resolveFile(scratchFolder, "file1.txt", opts);
+        final FileSystemOptions newOpts = file.getFileSystem().getFileSystemOptions();
+        assertSame(opts, newOpts);
+        assertTrue(builder.isVersioning(newOpts));
+        assertFalse(file.exists());
+        file.createFile();
+        assertTrue(file.exists());
+        assertSame(FileType.FILE, file.getType());
+        assertTrue(file.isFile());
+        assertEquals(0, file.getContent().getSize());
+        assertTrue(file.getContent().isEmpty());
+        assertFalse(file.isExecutable());
+        assertFalse(file.isHidden());
+        assertTrue(file.isReadable());
+        assertTrue(file.isWriteable());
+        Map<?, ?> map = file.getContent().getAttributes();
+        final String name = ((GenericURLFileName) file.getName()).getUserName();
+        assertTrue(map.containsKey(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
+        if (name != null) {
+            assertEquals(name, map.get(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
+        }
+        assertTrue(map.containsKey(VersionControlledResource.CHECKED_IN.toString()));
+
+        // Create the source file
+        final String content = "Here is some sample content for the file.  Blah Blah Blah.";
+
+        try (OutputStream os = file.getContent().getOutputStream()) {
+            os.write(content.getBytes(StandardCharsets.UTF_8));
+        }
+        assertSameContent(content, file);
+        map = file.getContent().getAttributes();
+        assertTrue(map.containsKey(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
+        if (name != null) {
+            assertEquals(name, map.get(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
+        }
+        assertTrue(map.containsKey(VersionControlledResource.CHECKED_IN.toString()));
+        builder.setVersioning(opts, false);
+    }
+
+    @Test
+    public void testVersioningWithCreator() throws Exception {
+        final FileObject scratchFolder = createScratchFolder();
+        final FileSystemOptions opts = scratchFolder.getFileSystem().getFileSystemOptions();
+        final Webdav4FileSystemConfigBuilder builder = (Webdav4FileSystemConfigBuilder) getManager()
+                .getFileSystemConfigBuilder("webdav");
+        builder.setVersioning(opts, true);
+        builder.setCreatorName(opts, "testUser");
+        final FileObject file = getManager().resolveFile(scratchFolder, "file1.txt", opts);
+        final FileSystemOptions newOpts = file.getFileSystem().getFileSystemOptions();
+        assertSame(opts, newOpts);
+        assertTrue(builder.isVersioning(newOpts));
+        assertFalse(file.exists());
+        file.createFile();
+        assertTrue(file.exists());
+        assertSame(FileType.FILE, file.getType());
+        assertTrue(file.isFile());
+        assertEquals(0, file.getContent().getSize());
+        assertTrue(file.getContent().isEmpty());
+        assertFalse(file.isExecutable());
+        assertFalse(file.isHidden());
+        assertTrue(file.isReadable());
+        assertTrue(file.isWriteable());
+        Map<?, ?> map = file.getContent().getAttributes();
+        final String name = ((GenericURLFileName) file.getName()).getUserName();
+        assertTrue(map.containsKey(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
+        assertEquals("testUser", map.get(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
+        if (name != null) {
+            assertTrue(map.containsKey(DeltaVConstants.COMMENT.toString()));
+            assertEquals("Modified by user " + name, map.get(DeltaVConstants.COMMENT.toString()));
+        }
+        assertTrue(map.containsKey(VersionControlledResource.CHECKED_IN.toString()));
+
+        // Create the source file
+        final String content = "Here is some sample content for the file.  Blah Blah Blah.";
+
+        try (OutputStream os = file.getContent().getOutputStream()) {
+            os.write(content.getBytes(StandardCharsets.UTF_8));
+        }
+        assertSameContent(content, file);
+        map = file.getContent().getAttributes();
+        assertTrue(map.containsKey(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
+        assertEquals("testUser", map.get(DeltaVConstants.CREATOR_DISPLAYNAME.toString()));
+        if (name != null) {
+            assertTrue(map.containsKey(DeltaVConstants.COMMENT.toString()));
+            assertEquals("Modified by user " + name, map.get(DeltaVConstants.COMMENT.toString()));
+        }
+        assertTrue(map.containsKey(VersionControlledResource.CHECKED_IN.toString()));
+        builder.setVersioning(opts, false);
+        builder.setCreatorName(opts, null);
+    }
+
+}
