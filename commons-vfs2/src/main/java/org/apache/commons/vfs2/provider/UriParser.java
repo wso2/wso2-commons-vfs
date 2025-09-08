efaa44c19b131d82430730ed0f516a67a090369b
GDLMadushanka <gdlmadushanka@gmail.com>
2025-09-08 14:20:55 +0530

    Fix issue in path normalization in VFS
    Fix issue in path normalization in VFS

--- commons-vfs2/src/main/java/org/apache/commons/vfs2/provider/UriParser.java
+++ commons-vfs2/src/main/java/org/apache/commons/vfs2/provider/UriParser.java
@@ -180,12 +180,25 @@ public final class UriParser {
      */
     public static boolean fixSeparators(final StringBuilder name) {
         boolean changed = false;
-        final int maxlen = name.length();
+        int maxlen = name.length();
         for (int i = 0; i < maxlen; i++) {
             final char ch = name.charAt(i);
             if (ch == TRANS_SEPARATOR) {
                 name.setCharAt(i, SEPARATOR_CHAR);
                 changed = true;
+            }
+            if (i < maxlen - 2 && name.charAt(i) == '%' && name.charAt(i + 1) == '2') {
+                if (name.charAt(i + 2) == 'f' || name.charAt(i + 2) == 'F') {
+                    name.setCharAt(i, SEPARATOR_CHAR);
+                    name.delete(i + 1, i + 3);
+                    maxlen -= 2;
+                    changed = true;
+                } else if (name.charAt(i + 2) == 'e' || name.charAt(i + 2) == 'E') {
+                    name.setCharAt(i, '.');
+                    name.delete(i + 1, i + 3);
+                    maxlen -= 2;
+                    changed = true;
+                }
             }
         }
         return changed;
